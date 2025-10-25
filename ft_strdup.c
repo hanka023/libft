3f72d36038ea82153da93ae2fc3bc7cb709ea4d7
hanka023 <128853526+hanka023@users.noreply.github.com>
2025-10-26 01:10:39 +0200

    Update ft_strdup.c

--- ft_strdup.c
+++ ft_strdup.c
@@ -9,6 +9,8 @@
 /*   Updated: 2025/06/30 14:10:02 by haskalov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+// The  strdup()  function  returns a pointer to a new string which is a duplicate of the string s.  Memory for the
+//       new string is obtained with malloc(3), and can be freed with free(3).
 
 #include "libft.h"
 
