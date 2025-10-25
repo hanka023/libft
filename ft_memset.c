25d70234b9822b48e5c40c9f4e80d3301c8a5ff5
hanka023 <128853526+hanka023@users.noreply.github.com>
2025-10-25 22:30:55 +0200

    Update ft_memset.c

--- ft_memset.c
+++ ft_memset.c
@@ -10,6 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+// The memset() function fills the first n bytes of the memory area pointed to by s with the constant byte c.
+
 #include "libft.h"
 
 void	*ft_memset(void *s, int c, size_t n)
