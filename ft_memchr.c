7294a5efa9966dd3e6b624435399f0cfe401ce1e
hanka023 <128853526+hanka023@users.noreply.github.com>
2025-10-25 22:22:40 +0200

    Update ft_memchr.c

--- ft_memchr.c
+++ ft_memchr.c
@@ -10,6 +10,10 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+// scan memory for a character
+// The  memchr()  function  scans  the initial n bytes of the memory area pointed to by s for the first instance of c.
+// Both c and the bytes of the memory area pointed to by s are interpreted as unsigned char.
+
 #include "libft.h"
 
 void	*ft_memchr(const void *s, int c, size_t n)
